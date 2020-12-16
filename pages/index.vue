324bc6d7e00e6e7d5df275bc65afd92a1b3f0da9
ahmed saad <saadamd@gmail.com>
2020-12-16 05:26:40 +0400

    updated

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,27 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <section
+      :style="{
+        background:
+          'url(https://images.unsplash.com/photo-1466838931486-92f3b5ff31a6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1487&q=80)',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRebate: 'no-rebate'
+      }"
+    >
+      <div class="wraper">
+        <h1>Nuxt Js Responsive Navbar</h1>
+      </div>
+    </section>
+    <section
+      :style="{
+        background:
+          'url(https://images.unsplash.com/photo-1559149251-e9a1dc89981d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80)',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center'
+      }"
+    ></section>
+  </div>
 </template>
 
 <script>
